da07be0dca7e15d081f7c09cd2aa1bdf5ddc0051
Chris Kozlowski <kozlowskicd@gmail.com>
2019-02-02 09:21:10 -0800

    fix folders/routes

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT
 
-app.use(express.static('./chat-app'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.send('./index.html');
-})
+});
+
+app.get('/chat', (req, res) => {
+  res.send('./chat-app/index.html');
+});
 
 app.listen(PORT, () => {
   console.log('Web Server up on port', PORT);
